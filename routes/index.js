63c52822726617f3b86b04a226cd13b94f8579e7
David Luque <david_92.8@hotmail.com>
2018-05-17 09:09:22 +0200

    MOD: Loop promises

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,24 @@ var router = express.Router();
 
 var axios = require("axios");
 
+var stores = [
+  {
+    name: "ebay",
+    countries: ['ca', 'us', 'in', 'my', 'ph', 'sg', 'at', 'be', 'fr', 'de', 'ie', 'it', 'nl', 'pl', 'es', 'ch', 'gb', 'au'],
+    token: 'DCDOTCRIXNGOEWOOFIVRQLSKFXCAXQLSRROJFCKXCEJHRRVVHEJZXOZFCQJVMTKB'
+  },
+  {
+    name: "google-shopping",
+    countries: ['br', 'ca', 'mx', 'us', 'in', 'jp', 'tr', 'at', 'be', 'cz', 'dk', 'fr', 'de', 'it', 'nl', 'no', 'pl', 'ru', 'es', 'se', 'ch', 'gb', 'au'],
+    token: 'LCGINPBIBHIWRJDONZEVARBQVIWUOUVCCTJKWIWNIOOBJVBAPPZOWBYJWGWLSKLK'
+  },
+  {
+    name: "amazon",
+    countries: ['ca', 'mx', 'us', 'in', 'fr', 'de', 'it', 'es', 'gb'],
+    token: 'NRHQYEFAWRIPPVOPDCOIHJABAMTHFRBFTGJNHHQYTAAZUTZBCMQKDCIXXYNVOPOC'
+  }
+];
+
 // Home page route.
 router.get("/", function (req, res) {
   res.send("This is the home page");
@@ -10,84 +28,42 @@ router.get("/", function (req, res) {
 
 // About page route.
 router.post("/llamada", async function (req, res) {
-  var country = req.query.country;
-  var values = req.query.values;
-  var token = 'NRHQYEFAWRIPPVOPDCOIHJABAMTHFRBFTGJNHHQYTAAZUTZBCMQKDCIXXYNVOPOC';
-  var token2 = 'LCGINPBIBHIWRJDONZEVARBQVIWUOUVCCTJKWIWNIOOBJVBAPPZOWBYJWGWLSKLK';
-  var token3 = 'DCDOTCRIXNGOEWOOFIVRQLSKFXCAXQLSRROJFCKXCEJHRRVVHEJZXOZFCQJVMTKB';
-  const result = await Promise.all([
-    llamadaLibro(country, values, token3, "ebay")
-    .then(async id => {
-      try{
-        var statusId = 'working';
-        while((statusId != 'finished') && (statusId != 'undefined')){
-          //statusId = await segundaLlamada(id, "1", token3);
-          var [parents] = await Promise.all([
-            statusId = await segundaLlamada(id, "1", token3),
-            sleep(2000)
-          ]);
-          console.log("ESTADO 1 " + statusId);
-        }
-        return await terceraLlamada(id, token3);
-      }catch(error){
-        console.log(error)
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    }),
-    llamadaLibro(country, values, token2, "google-shopping")
-    .then(async id => {
-      try{
-          var statusId = 'working';
-          while((statusId != 'finished') && (statusId != 'undefined')){
-            //statusId = await segundaLlamada(id, "2", token2);
-            var [parents] = await Promise.all([
-              statusId = await segundaLlamada(id, "2", token2),
-              sleep(2000)
-            ]);
-            console.log("ESTADO 2 " + statusId);
+  const country = req.query.country;
+  const values = req.query.values;
+  const promises = [];
+  stores.forEach(item => {
+    if (item.countries.includes(country)) {
+      promises.push(llamadaLibro(country, values, item.token, item.name)
+        .then(async id => {
+          try {
+            let statusId = 'working';
+            while ((statusId != 'finished') && (statusId != 'undefined')) {
+              let [parents] = await Promise.all([
+                statusId = await segundaLlamada(id, item.name, item.token),
+                sleep(2000)
+              ]);
+              console.log("ESTADO 1 " + statusId);
+            }
+            return await terceraLlamada(id, item.token);
+          } catch (error) {
+            console.log(error)
           }
-          return await terceraLlamada(id, token2);
-      }catch(error){
-        console.log(error)
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    }),
-    llamadaLibro(country, values, token, "amazon")
-    .then(async id => {
-      try{
-        var statusId = 'working';
-        while((statusId != 'finished') && (statusId != 'undefined')){
-          //statusId = await segundaLlamada(id, "3", token);
-          var [parents] = await Promise.all([
-            statusId = await segundaLlamada(id, "3", token),
-            sleep(2000)
-          ]);
-          console.log("ESTADO 3 " + statusId);
-        }
-        return await terceraLlamada(id, token);
-      }catch(error){
-        console.log(error)
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  ]);
-
-  try{
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      );
+    }
+  })
+  const result = await Promise.all(promises);
+  try {
     console.log("<------------ HA TERMINADO ------------>")
     console.log(result);
-    //res.send(JSON.stringify(result));
     var finalResult = checkResult(result);
     finalResult.offers.sort(pWShipping)
     console.log(finalResult);
-    //res.send(JSON.stringify(finalResult));
-    res.send({results: finalResult});
-  }catch(error){
+    res.send({ results: finalResult });
+  } catch (error) {
     console.log(error);
   }
 });
@@ -98,6 +74,7 @@ function sleep(ms) {
 
 function llamadaLibro(country, values, token, source) {
   console.log("Primera llamada");
+  console.log(token + ' ' + source)
   return axios
     .post("https://api.priceapi.com/jobs", {
       token: token,
@@ -116,8 +93,8 @@ function llamadaLibro(country, values, token, source) {
     });
 }
 
-function segundaLlamada(job_id, num, token) {
-  console.log("Segunda llamada" + num);
+function segundaLlamada(job_id, name, token) {
+  console.log("Segunda llamada " + name + ' ' + token);
   return axios
     .get("https://api.priceapi.com/jobs/" + job_id, {
       params: {
@@ -125,7 +102,6 @@ function segundaLlamada(job_id, num, token) {
       }
     })
     .then(res => {
-      console.log(res.data.status)
       return res.data.status;
     })
     .catch(e => {
@@ -149,7 +125,7 @@ function terceraLlamada(job_id, token) {
     });
 }
 
-function checkResult(result){
+function checkResult(result) {
   var resClean = {
     name: '',
     imageUrl: '',
@@ -158,36 +134,36 @@ function checkResult(result){
   };
 
   // Ebay
-  if(result[0].products[0].success){
+  if (result[0].products[0].success) {
     loopPush(result[0].products[0].offers, resClean.offers, 'ebay');
   }
   // Google Shopping
-  if(result[1].products[0].success){
+  if (result[1].products[0].success) {
     loopPush(result[1].products[0].offers, resClean.offers, 'googleShopping');
   }
   // Amazon
-  if(result[2].products[0].success){
+  if (result[2].products[0].success) {
     loopPush(result[2].products[0], resClean.offers, 'amazon');
   }
 
-  if(result[1].products[0].success){
+  if (result[1].products[0].success) {
     resClean.name = result[1].products[0].name;
     resClean.description = result[1].products[0].description;
     resClean.imageUrl = result[1].products[0].image_url;
-  }else if(result[2].products[0].success){
+  } else if (result[2].products[0].success) {
     resClean.name = result[2].products[0].name;
-  }else if(result[0].products[0].success){
+  } else if (result[0].products[0].success) {
     resClean.name = result[0].products[0].offers[0].name;
-  }else{
+  } else {
     resClean.name = "Product not found";
   }
   return resClean;
-  
+
 }
 
-function loopPush(arrayOrigin, arrayDestiny, shopName){
+function loopPush(arrayOrigin, arrayDestiny, shopName) {
 
-  if(shopName == 'amazon'){
+  if (shopName == 'amazon') {
 
     var offerExaple = {
       price: '',
@@ -203,9 +179,9 @@ function loopPush(arrayOrigin, arrayDestiny, shopName){
     offerExaple.shopName = arrayOrigin.offers[0].shop_name;
     offerExaple.url = arrayOrigin.url;
     arrayDestiny.push(offerExaple);
-  }else{
+  } else {
     console.log(arrayOrigin.length)
-    for(var i = 0; i < arrayOrigin.length; i++){
+    for (var i = 0; i < arrayOrigin.length; i++) {
       console.log(i);
       console.log(arrayOrigin[i]);
       var offerExaple = {
@@ -226,7 +202,7 @@ function loopPush(arrayOrigin, arrayDestiny, shopName){
   }
 }
 
-function pWShipping(a,b){
+function pWShipping(a, b) {
   return a.priceWithShipping - b.priceWithShipping;
 }
 
